52bbfa87d059faa925c6410b4fed94dc8c6e0401
Patric Pereira <patric.franca@petanjo.com>
2026-08-06 14:38:55 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Patric Pereira on 06/08/26.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
