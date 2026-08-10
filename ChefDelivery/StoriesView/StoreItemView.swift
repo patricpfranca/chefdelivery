3054e03958fe8fdbfafebaadc3576e8e6f4fe863
Patric Pereira <patric.franca@petanjo.com>
2026-08-09 21:06:13 -0300

    Add StoreItemView component for displaying individual order items in the ChefDelivery app

---
+++ ChefDelivery/StoriesView/StoreItemView.swift
@@ -0,0 +1,32 @@
+//
+//  StoreItemView.swift
+//  ChefDelivery
+//
+//  Created by Patric Pereira on 09/08/26.
+//
+
+import SwiftUI
+
+struct StoreItemView: View {
+    
+    let order: OrderType
+    
+    var body: some View {
+        HStack {
+            Image(order.image)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(25)
+                .frame(width: 50, height: 50)
+            
+            VStack {
+                Text(order.name)
+                    .font(.subheadline)
+            }
+        }
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    StoreItemView(order: OrderType(id: 1, name: "Monstro Burguer", image: "monstro-burger-logo"))
+}
