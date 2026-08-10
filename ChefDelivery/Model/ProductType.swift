a529d10483a970bcdf9b46cdcab9eda317c9c6a0
Patric Pereira <patric.franca@petanjo.com>
2026-08-10 14:11:12 -0300

    Refactor data models to introduce StoreType and ProductType, updating mock data and views to support new structure for store and product representation in the ChefDelivery app.

---
+++ ChefDelivery/Model/ProductType.swift
@@ -0,0 +1,16 @@
+//
+//  ProductType.swift
+//  ChefDelivery
+//
+//  Created by Patric Pereira on 10/08/26.
+//
+
+import Foundation
+
+struct ProductType: Identifiable {
+    let id: Int
+    let name: String
+    let description: String
+    let image: String
+    let price: Double
+}
