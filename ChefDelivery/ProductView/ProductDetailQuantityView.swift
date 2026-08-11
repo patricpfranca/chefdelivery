52f8a7de88f82acb5914649d8016b6e785b1c926
Patric Pereira <patric.franca@petanjo.com>
2026-08-11 11:55:43 -0300

    Refactor ProductDetailQuantityView to use a Binding for product quantity, allowing for better state management. Update ProductDetailView to pass the product quantity as a binding, enhancing the interaction between components.

--- ChefDelivery/ProductView/ProductDetailQuantityView.swift
+++ ChefDelivery/ProductView/ProductDetailQuantityView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ProductDetailQuantityView: View {
     
-    @State private var productQuantity = 1
+    @Binding var productQuantity: Int
     
     var body: some View {
         VStack(spacing: 16) {
@@ -46,5 +46,5 @@ struct ProductDetailQuantityView: View {
 }
 
 #Preview {
-    ProductDetailQuantityView()
+    ProductDetailQuantityView(productQuantity: .constant(1))
 }
