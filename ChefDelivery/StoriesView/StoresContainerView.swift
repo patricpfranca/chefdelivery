64fb1d7d36f303b326696ea829862f197a1b7c38
Patric Pereira <patric.franca@petanjo.com>
2026-08-11 08:52:00 -0300

    Add rating filter to StoresContainerView, allowing users to filter stores by star ratings. Implemented a menu for selecting rating criteria alongside the store title for improved user experience.

--- ChefDelivery/StoriesView/StoresContainerView.swift
+++ ChefDelivery/StoriesView/StoresContainerView.swift
@@ -10,11 +10,32 @@ import SwiftUI
 struct StoresContainerView: View {
     
     let title = "Lojas"
+    @State private var ratingFilter = 0
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text(title)
-                .font(.headline)
+            HStack {
+                Text(title)
+                    .font(.headline)
+                
+                Spacer()
+                
+                Menu("Filtrar") {
+
+                    ForEach(1...5, id: \.self) { rating in
+                        Button {
+                            ratingFilter = rating
+                        } label: {
+                            let textStarPlural = rating > 1 ? "estrelas" : "estrela"
+                            Text("\(rating) \(textStarPlural) ou mais")
+                        }
+
+                    }
+                    
+                }
+                .foregroundStyle(.black)
+            }
+            
             
             VStack(alignment: .leading, spacing: 30) {
                 ForEach(storesMock) { mock in
